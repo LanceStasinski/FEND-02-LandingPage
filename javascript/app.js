9e781085e9ba10ac5b749cc8e9ba6143ff906eb0
Lance Stasinski <57465065+LanceStasinski@users.noreply.github.com>
2021-04-20 20:31:33 -0400

    Working on setting reactive nav links
    Added function that adds a class if a section is in view.

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,16 @@
+//function that detects if element is in viewport. Modified from https://www.javascripttutorial.net/dom/css/check-if-an-element-is-visible-in-the-viewport/
+const inViewport = function(element) {
+  //get an element's position and position relative to the viewport
+  const rect = element.getBoundingClientRect();
+  //return true if top and left of an element are in the window and the bottom and right are either in the window or client within a window
+  return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
 //build the menu using section information
 
 const navigationList = document.getElementById('nav-list');
@@ -22,6 +35,7 @@ for (i = 1; i <= numberOfCards; i++) {
   newLi.appendChild(liText);
   //give the new list item an ID
   let liID = "#list-item" + i;
+  liID.id = liID;
   //apend the list item to the navigation list
   navigationList.appendChild(newLi);
   //store the linked section as a variable
@@ -35,7 +49,40 @@ for (i = 1; i <= numberOfCards; i++) {
     })
   });
 
-  newLi.addEventListener( ,)
+  document.addEventListener("scroll", function () {
+    if (inViewport(target)) {
+      newLi.classList.add("active");
+    } else {
+      newLi.classList.remove("active");
+    }
+  })
 }
 
 //if section is in view, set navbar link to active
+/*
+for (let i = 1; i <= numberOfCards; i++){
+  let sectionInView = document.getElementById("section" + i);
+  let navItem = document.getElementById("#list-item" + i)
+  document.addEventListener("scroll", function () {
+    if (inViewport(sectionInView)) {
+      navItem.classList.add("active");
+    } else {
+      navItem.classList.remove("active");
+    }
+  });
+}
+
+
+setTimeout(function () {
+  for (let i = 1; i <= numberOfCards; i++){
+    let sectionInView = document.getElementById("section" + i);
+    let navName = "#list-item" + i;
+    let navItem = document.querySelector(navName);
+    if (inViewport(sectionInView)) {
+      navItem.classList.add("active");
+    } else {
+      navItem.classList.remove("active");
+    }
+  };
+}, 0)
+*/
